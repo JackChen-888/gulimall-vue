727858dbfb8fb75baac0ba656c05f67e302e52da
liuchen525@126.com <liuchen525@126.com>
2020-12-01 17:54:48 +0800

    商品服务列表的删除功能

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -1,29 +1,55 @@
 <template>
-  <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+  <el-tree :data="menus" :props="defaultProps"
+           :expand-on-click-node="false"
+           show-checkbox
+           node-key="catId">
+    <span class="custom-tree-node" slot-scope="{ node, data }">
+        <span>{{ node.label }}</span>
+        <span>
+          <el-button v-if="node.level <= 2"
+                     type="text"
+                     size="mini"
+                     @click="() => append(data)">
+            Append
+          </el-button>
+          <el-button v-if="node.childNodes.length==0"
+                     type="text"
+                     size="mini"
+                     @click="() => remove(node, data)">
+            Delete
+          </el-button>
+        </span>
+      </span>
+  </el-tree>
 </template>
 
 <script>
   export default {
     data() {
       return {
-        data: [],
+        menus: [],
         defaultProps: {
           children: 'children',
-          label: 'label'
+          label: 'name'
         }
       };
     },
     methods: {
-      handleNodeClick(data) {
-        console.log(data);
-      },
       getMenus() {
         this.$http({
-          url: this.$http.adornUrl('product/category/list/tree'),
+          url: this.$http.adornUrl('/product/category/list/tree'),
           method: 'get'
-        }).then(data => {
-          console.log("成功获取到菜单数据", data)
+        }).then(({data}) => {
+          console.log("成功获取到菜单数据", data.data)
+          this.menus = data.data;
         })
+      },
+      append(data) {
+        console.log("添加一个节点", data)
+      },
+
+      remove(node, data) {
+        console.log("删除一个节点", data)
       }
     },
     created() {
